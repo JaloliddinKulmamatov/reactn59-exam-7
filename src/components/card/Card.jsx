7ceb1a41d654f65f3d6add96f45551d18d6c56db
JaloliddinKulmamatov <itjalol2010@gmail.com>
2024-07-19 17:14:39 +0500

    fix the cart

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,12 @@
 import { FaCartShopping } from "react-icons/fa6";
-
 import Button from "../button/Button";
 import styles from "./Card.module.scss";
 import { Link } from "react-router-dom";
 
-const Card = ({ product, cart, setCart }) => {
+const Card = ({ product,setCart,cart }) => {
   return (
     <div className={styles.cardMain}>
-      <div className={styles.cardTop }>
+      <div className={styles.cardTop}>
         <img src={product.image_url} alt={product.product_name} />
       </div>
       <div className={styles.cardBottom}>
@@ -15,24 +14,22 @@ const Card = ({ product, cart, setCart }) => {
           <Link to={`/products/${product.id}`}>{product.name}</Link>
         </h4>
         <p>{product.description}</p>
-
         <div className={styles.colors}>
           {product.color_options.map((color, index) => (
             <div
               key={index}
-              style={{
-                background: color,
-              }}
+              style={{ background: color }}
               className={styles.color}
             />
           ))}
         </div>
-
         <strong>{product.price}$</strong>
         <div>
-          <Button >
-            <FaCartShopping color="white"/>
-            <span style={{ marginLeft: "0.8em", color:"#fff" }}>Add to Cart</span>
+          <Button onClick={() => setCart([...cart, product])}>
+            <FaCartShopping color="white" />
+            <span style={{ marginLeft: "0.8em", color: "#fff" }}>
+              Add to Cart
+            </span>
           </Button>
         </div>
       </div>
